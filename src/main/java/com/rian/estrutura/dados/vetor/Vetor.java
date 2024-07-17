3829132005b1f21fb65cb8915bffa9a797c127e1
Rian H. S. Ezequias <rianhenrique0408@gmail.com>
2024-07-16 21:20:40 -0300

    first commit

---
+++ src/main/java/com/rian/estrutura/dados/vetor/Vetor.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.rian.estrutura.dados.vetor;
+
+/**
+ *
+ * @author rianh
+ */
+public class Vetor {
+
+    private String[] elementos;
+
+    public Vetor(int capacidade) {
+        this.elementos = new String[capacidade];
+    }
+
+}
---
+++ src/main/java/com/rian/estrutura/dados/vetor/teste/Aula02.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.rian.estrutura.dados.vetor.teste;
+
+import com.rian.estrutura.dados.vetor.Vetor;
+
+/**
+ *
+ * @author rianh
+ */
+public class Aula02 {
+
+    public static void main(String[] args) {
+
+        Vetor vetor = new Vetor(5);
+
+        int i = 0;
+        i++;
+    }
+
+}
