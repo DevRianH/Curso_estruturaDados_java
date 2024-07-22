8d1b9cb27bbd11d0f5eecb184005cad8ac86d350
Rian H. S. Ezequias <rhezequias@coderp.ribeiraopreto.sp.gov.br>
2024-07-22 08:43:01 -0300

    ta salvado

---
+++ src/main/java/com/rian/estrutura/dados/vetor/VetorObjetos.java
@@ -0,0 +1,123 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.rian.estrutura.dados.vetor;
+
+/**
+ *
+ * @author rianh
+ */
+public class VetorObjetos {
+
+    private Object[] elementos;
+    private int tamanho;
+
+    public VetorObjetos(int capacidade) {
+        this.elementos = new Object[capacidade];
+        this.tamanho = 0;
+    }
+
+    public boolean adiciona(Object elemento) {
+        this.aumentaCapacidade();
+        if (this.tamanho < this.elementos.length) {
+            this.elementos[this.tamanho] = elemento;
+            this.tamanho++;
+            return true;
+        }
+        return false;
+    }
+
+    // 0 1 2 3 4 5 6 = tamanho é 5
+    // B C E F G + +
+    //
+    public boolean adiciona(int posicao, Object elemento) {
+
+        if (!(posicao >= 0 && posicao < tamanho)) {
+            throw new IllegalArgumentException("Posição inválida");
+        }
+
+        this.aumentaCapacidade();
+
+        // mover todos os elementos
+        for (int i = this.tamanho - 1; i >= posicao; i--) {
+            this.elementos[i + 1] = this.elementos[i];
+        }
+        this.elementos[posicao] = elemento;
+        this.tamanho++;
+
+        return true;
+    }
+
+    private void aumentaCapacidade() {
+        if (this.tamanho == this.elementos.length) {
+            Object[] elementosNovos = new Object[this.elementos.length * 2];
+            for (int i = 0; i < this.elementos.length; i++) {
+                elementosNovos[i] = this.elementos[i];
+            }
+            this.elementos = elementosNovos;
+        }
+
+    }
+
+    // B D E E F -> posição a ser removida é 1 (G)
+    // 0 1 2 3 4 -> tamanho é 5
+    // vetor[1] = vetor[2]
+    // vetor[2] = vetor[3]
+    // vetor[3] = vetor[4]
+    public void remove(int posicao) {
+        if (!(posicao >= 0 && posicao < tamanho)) {
+            throw new IllegalArgumentException("Posição inválida");
+        }
+
+        for (int i = posicao; i < this.tamanho - 1; i++) {
+            this.elementos[i] = this.elementos[i + 1];
+        }
+        tamanho--;
+    }
+
+    public int tamanho() {
+        return this.tamanho;
+    }
+
+    public Object busca(int posicao) {
+
+        if (!(posicao >= 0 && posicao < tamanho)) {
+            throw new IllegalArgumentException("Posição inválida");
+        }
+
+        return this.elementos[posicao];
+    }
+
+    @Override
+    public String toString() {
+
+        StringBuilder s = new StringBuilder();
+
+        s.append("[");
+
+        for (int i = 0; i < this.tamanho - 1; i++) {
+            s.append(this.elementos[i]);
+            s.append(" ,");
+
+        }
+
+        if (this.tamanho > 0) {
+            s.append(this.elementos[this.tamanho - 1]);
+
+        }
+
+        s.append("]");
+
+        return s.toString();
+    }
+
+    public int busca(Object elemento) {
+        for (int i = 0; i < this.tamanho; i++) {
+            if (this.elementos[i].equals(elemento)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
