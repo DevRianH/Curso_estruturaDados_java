570a3b8a39441056e8838ab765b664dc017279ab
Rian H. S. Ezequias <rianhenrique0408@gmail.com>
2024-07-30 22:20:47 -0300

    começo sobre pilha

---
+++ src/main/java/com/rian/estrutura/dados/base/EstruturaEstatica.java
@@ -0,0 +1,90 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.rian.estrutura.dados.base;
+
+/**
+ *
+ * @author rianh
+ */
+public class EstruturaEstatica<T> {
+
+    private T[] elementos;
+    private int tamanho;
+
+    public EstruturaEstatica(int capacidade) {
+        this.elementos = elementos;
+        this.tamanho = tamanho;
+    }
+
+    public EstruturaEstatica() {
+    }
+
+    protected boolean adiciona(T elemento) {
+        this.aumentaCapacidade();
+        if (this.tamanho < this.elementos.length) {
+            this.elementos[this.tamanho] = elemento;
+            this.tamanho++;
+            return true;
+        }
+        return false;
+    }
+
+    protected boolean adiciona(int posicao, T elemento) {
+
+        if (!(posicao >= 0 && posicao < tamanho)) {
+            throw new IllegalArgumentException("Posição inválida");
+        }
+
+        this.aumentaCapacidade();
+
+        // mover todos os elementos
+        for (int i = this.tamanho - 1; i >= posicao; i--) {
+            this.elementos[i + 1] = this.elementos[i];
+        }
+        this.elementos[posicao] = elemento;
+        this.tamanho++;
+
+        return true;
+    }
+
+    private void aumentaCapacidade() {
+        if (this.tamanho == this.elementos.length) {
+            T[] elementosNovos = (T[]) new Object[this.elementos.length * 2];
+            for (int i = 0; i < this.elementos.length; i++) {
+                elementosNovos[i] = this.elementos[i];
+            }
+            this.elementos = elementosNovos;
+        }
+
+    }
+
+    public int tamanho() {
+        return this.tamanho;
+    }
+
+    @Override
+    public String toString() {
+
+        StringBuilder s = new StringBuilder();
+
+        s.append("[");
+
+        for (int i = 0; i < this.tamanho - 1; i++) {
+            s.append(this.elementos[i]);
+            s.append(" ,");
+
+        }
+
+        if (this.tamanho > 0) {
+            s.append(this.elementos[this.tamanho - 1]);
+
+        }
+
+        s.append("]");
+
+        return s.toString();
+    }
+
+}
---
+++ src/main/java/com/rian/estrutura/dados/vetor/Lista2.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.rian.estrutura.dados.vetor;
+
+import com.rian.estrutura.dados.base.EstruturaEstatica;
+
+/**
+ *
+ * @author rianh
+ */
+public class Lista2<T> extends EstruturaEstatica<T> {
+
+    public Lista2() {
+        super();
+    }
+
+    public Lista2(int capacidade) {
+        super(capacidade);
+    }
+
+    public boolean adiciona(T elemento) {
+        return super.adiciona(elemento);
+    }
+
+    public boolean adiciona(int posicao, T elemento) {
+        return super.adiciona(posicao, elemento);
+    }
+}
